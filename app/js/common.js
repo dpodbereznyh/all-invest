9e1c4576a7ae3140fa2ece7bccc0c967e2ad2497
dpodbereznyh <dpodbereznyh@gmail.com>
2020-03-27 11:54:02 +0500

    Все в app

---
+++ app/js/common.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+    // Menu
+    $('.header__menu_list>li').click(function (e) {
+        $('.' + $(this).data('ddm')).toggleClass('ddm-active');
+    });
+
+    // SmartMenus init
+    $('#main-menu').smartmenus({
+        subMenusSubOffsetX: 1,
+        subMenusSubOffsetY: -8
+    });
+
+    // SmartMenus mobile menu toggle button
+    // SmartMenus init
+    $(function() {
+        $('#main-menu').smartmenus({
+            subMenusSubOffsetX: 1,
+            subMenusSubOffsetY: -8
+        });
+    });
+
+// SmartMenus mobile menu toggle button
+    $(function() {
+        $('#main-menu').smartmenus();
+
+    });
+
+
+    $('.fa-bars').click(function (e) {
+        e.preventDefault();
+        $('.ddm__wrap').toggleClass('ddm-active');
+    });
+
+    // Submenu
+    $('.sub-menu').hide();
+    $('.ddm__title').mouseover(function (e) {
+        $(this).siblings('.sub-menu').show();
+    });
+    $('.ddm__list').mouseleave(function (e) {
+        $('.sub-menu').hide();
+    });
+
+    // Fancybox
+    $(document).ready(function() {
+        $(".fancybox").fancybox();
+    });
+
+    // Techinfo
+    $('.techinfo__specifications').hide();
+    $('.techinfo__nav_specific').click(function (e) {
+        if ($('.techinfo__description').css('display') == 'block') {
+            $('.techinfo__description').css('display', 'none');
+            $('.techinfo-active_desc').css('display', 'none');
+            $('.techinfo__specifications').css('display', 'block');
+            $('.techinfo-active_spec').css('display', 'block');
+        }
+        else {
+            $('.techinfo__specifications').css('display', 'none');
+            $('.techinfo-active_spec').css('display', 'none');
+        }
+    });
+    $('.techinfo__nav_desc').click(function (e) {
+        if ($('.techinfo__description').css('display') == 'none') {
+            $('.techinfo__description').css('display', 'block');
+            $('.techinfo-active_desc').css('display', 'block');
+            $('.techinfo__specifications').css('display', 'none');
+            $('.techinfo-active_spec').css('display', 'none');
+        }
+        else {
+            $('.techinfo__specifications').css('display', 'none');
+            $('.techinfo-active_spec').css('display', 'none');
+        }
+    });
+
+
+    // $('.ddm__list').mouseleave(function (e) {
+    //     $('.sub-menu').hide();
+    // });
+
+    // Map
+    ymaps.ready(init);
+
+    function init() {
+        var myMap = new ymaps.Map("map", {
+            center: [56.12657906861558, 40.4115845],
+            zoom: 16,
+            controls: []
+        });
+        var myPlacemark = new ymaps.Placemark([56.12657906861558, 40.4115845], {}, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map-marker.png',
+            iconImageSize: [40, 40],
+            iconImageOffset: [-3, -42]
+        });
+        myMap.geoObjects.add(myPlacemark);
+
+        myMap.behaviors.disable(['scrollZoom'])
+    }
+    // Projects Nav
+    $('.image__nav').on('click', function () {
+        $('.img__after').toggleClass('img__hide');
+        $('.image__nav').toggleClass('nav__swipe')
+        $('.img__before').toggleClass('img__top')
+    })
+    // Popap
+    $('.header__nav_btn').on("click", function () {
+        $('#popap__header_btn').show();
+    });
+    $('.popap__close').on("click", function () {
+        $('#popap__header_btn').hide();
+    });
+
+    // До/После
+    $ ( ".projects__list_item_img" ) .twentytwenty ();
+
+});
+
